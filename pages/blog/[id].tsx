d7ef0fd2b14fbc36ca8e921ac5bc3e9eaa98b9a3
Linus Björklund <weedzcokie@gmail.com>
2022-07-22 15:38:48 +0200

    Remove unused import

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,7 +1,6 @@
 import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';;
+import Link from 'next/link';
 import styles from '../../styles/Home.module.css'
 
 interface Props {
